3f5b1a86d1111f7d02a63b9c051425ab856348ca
Eric Hamilton <dilvie@dilvie.com>
2012-09-30 05:03:50 -0700

    Added explanatory comments.

--- random-uniqueness.js
+++ random-uniqueness.js
@@ -1,3 +1,18 @@
+/**
+ * Demonstrating the weakness of relying on random data as a
+ * uniqueness guarantee. Nice in theory, but unreliable in practice.
+ **/
+
+/* "...after generating 1 billion UUIDs every second for the next 100 
+ * years, the probability of creating just one duplicate would be 
+ * about 50%" */
+// ...
+/* "However, these probabilities only hold when the UUIDs are 
+  * generated using sufficient entropy." */
+
+// http://en.wikipedia.org/wiki/Universally_unique_identifier
+
+
 function rollDie () {
   // Randomly generated UUIDs have 122 random bits
   return Math.ceil(Math.random() * Math.pow(2, 122));
@@ -25,7 +40,10 @@ function rollTest() {
   finish = new Date();
 
   if (collision) {
-    console.log('Collision detected after ' + (finish - start).toString() + 'ms, ' + rolls + ' ids.');
+    // Theoretically so rare as to be practically impossible...
+    // But it happens all the time in V8.
+    console.log('Collision detected after ' +
+      (finish - start).toString() + 'ms, ' + rolls + ' ids.');
   } else {
     console.log('Limit reach. No collisions detected.');
   }
